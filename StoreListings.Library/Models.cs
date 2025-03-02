a35b5039190a5a728dbfacbeabdc4cbf95170e1c
Jishnu M <mjishnu@proton.me>
2025-03-02 13:49:17 +0530

    switch to sealed classes

--- StoreListings.Library/Models.cs
+++ StoreListings.Library/Models.cs
@@ -1,17 +1,35 @@
 ﻿namespace StoreListings.Library;
 
-public record struct Image(
-    string Url,
-    string BackgroundColor,
-    int Height,
-    int Width
-);
+public sealed class Image
+{
+    public string Url { get; set; }
+    public string BackgroundColor { get; }
+    public int Height { get; }
+    public int Width { get; }
 
-public record Card(
-    string ProductId,
-    string Title,
-    string? DisplayPrice,
-    double? AverageRating,
-    Image Image
+    public Image(string url, string backgroundColor, int height, int width)
+    {
+        Url = url;
+        BackgroundColor = backgroundColor;
+        Height = height;
+        Width = width;
+    }
+}
 
-);
+public sealed class Card
+{
+    public string ProductId { get; }
+    public string Title { get; }
+    public string? DisplayPrice { get; }
+    public double? AverageRating { get; }
+    public Image Image { get; }
+
+    public Card(string productId, string title, string? displayPrice, double? averageRating, Image image)
+    {
+        ProductId = productId;
+        Title = title;
+        DisplayPrice = displayPrice;
+        AverageRating = averageRating;
+        Image = image;
+    }
+}
