efbca3d734913b9741ff4cf459b477104f1e59bd
Jishnu M <mjishnu@proton.me>
2025-03-30 22:24:24 +0530

    use primary constructor for models

--- StoreListings.Library/Models.cs
+++ StoreListings.Library/Models.cs
@@ -1,41 +1,24 @@
 ﻿namespace StoreListings.Library;
 
-public sealed class Image
+public sealed class Image(string url, string backgroundColor, int height, int width)
 {
-    public string Url { get; set; }
-    public string BackgroundColor { get; }
-    public int Height { get; }
-    public int Width { get; }
-
-    public Image(string url, string backgroundColor, int height, int width)
-    {
-        Url = url;
-        BackgroundColor = backgroundColor;
-        Height = height;
-        Width = width;
-    }
+    public string Url { get; set; } = url;
+    public string BackgroundColor { get; } = backgroundColor;
+    public int Height { get; } = height;
+    public int Width { get; } = width;
 }
 
-public sealed class Card
+public sealed class Card(
+    string productId,
+    string title,
+    string? displayPrice,
+    double? averageRating,
+    Image image
+)
 {
-    public string ProductId { get; }
-    public string Title { get; }
-    public string? DisplayPrice { get; }
-    public double? AverageRating { get; }
-    public Image Image { get; }
-
-    public Card(
-        string productId,
-        string title,
-        string? displayPrice,
-        double? averageRating,
-        Image image
-    )
-    {
-        ProductId = productId;
-        Title = title;
-        DisplayPrice = displayPrice;
-        AverageRating = averageRating;
-        Image = image;
-    }
+    public string ProductId { get; } = productId;
+    public string Title { get; } = title;
+    public string? DisplayPrice { get; } = displayPrice;
+    public double? AverageRating { get; } = averageRating;
+    public Image Image { get; } = image;
 }
